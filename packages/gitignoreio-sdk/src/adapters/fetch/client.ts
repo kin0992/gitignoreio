a490c001a139245582ca65a1629f118098ac7d80
Marco Comi <9998393+kin0992@users.noreply.github.com>
2025-09-11 22:18:32 +0200

    Lint code

--- packages/gitignoreio-sdk/src/adapters/fetch/client.ts
+++ packages/gitignoreio-sdk/src/adapters/fetch/client.ts
@@ -6,14 +6,20 @@ export class DefaultHttpClient implements HttpClient {
   get(url: URL) {
     return ResultAsync.fromPromise(
       fetch(url),
-      (err) => new Error(`Failed to fetch data from ${url}: ${err instanceof Error ? err.message : String(err)}`),
+      (err) =>
+        new Error(
+          `Failed to fetch data from ${url}: ${err instanceof Error ? err.message : String(err)}`,
+        ),
     ).andThen((response) => {
       if (!response.ok) {
         return errAsync(new Error(`HTTP error! status: ${response.status}`));
       }
       return ResultAsync.fromPromise(
         response.text(),
-        () => new Error(`Failed to read response text from ${url} (status: ${response.status})`),
+        () =>
+          new Error(
+            `Failed to read response text from ${url} (status: ${response.status})`,
+          ),
       );
     });
   }
