659cbd2b25deb1a1dc82a5eec115f75accb8ccef
Marco Comi <9998393+kin0992@users.noreply.github.com>
2025-09-27 15:18:40 +0200

    Rename test file

--- packages/gitignoreio-sdk/src/adapters/__tests__/sdk.test.ts
+++ packages/gitignoreio-sdk/src/adapters/__tests__/sdk.test.ts
@@ -2,7 +2,7 @@ import { errAsync, okAsync } from 'neverthrow';
 import { describe, expect, it } from 'vitest';
 
 import { makeMockHttpClient } from '../../__tests__/data.js';
-import { GitIgnoreSDK } from '../sdk';
+import { GitIgnoreSDK } from '../sdk.js';
 
 describe('GitIgnoreSDK', () => {
   describe('generate', () => {
@@ -35,6 +35,36 @@ describe('GitIgnoreSDK', () => {
         new URL(
           'https://www.toptal.com/developers/gitignore/api/node%2Cpython',
         ),
+      );
+    });
+  });
+
+  describe('list', () => {
+    it('should return the list of technologies', async () => {
+      const mockContent = 'a\nb,c';
+      const mockHttpClient = makeMockHttpClient();
+      mockHttpClient.get.mockReturnValueOnce(okAsync(mockContent));
+      const sdk = new GitIgnoreSDK(mockHttpClient);
+
+      const result = await sdk.listTechnologies();
+
+      expect(mockHttpClient.get).toHaveBeenCalledWith(
+        new URL('https://www.toptal.com/developers/gitignore/api/list'),
+      );
+      expect(result).toStrictEqual(['a', 'b', 'c']);
+    });
+
+    it('should throw an error when HTTP client fails', async () => {
+      const mockHttpClient = makeMockHttpClient();
+      mockHttpClient.get.mockReturnValueOnce(errAsync(new Error('anError')));
+      const sdk = new GitIgnoreSDK(mockHttpClient);
+
+      await expect(sdk.listTechnologies()).rejects.toStrictEqual(
+        new Error('There was an error fetching data from gitignore.io'),
+      );
+
+      expect(mockHttpClient.get).toHaveBeenCalledWith(
+        new URL('https://www.toptal.com/developers/gitignore/api/list'),
       );
     });
   });
